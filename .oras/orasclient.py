dd72b8e708b7fb23091e7aa39842e231f0284e07
Aryan Rajoria <aryanrajoria1003@gmail.com>
2024-11-06 09:38:12 +0100

    fix orasclient.py
    Signed-off-by: Aryan Rajoria <aryanrajoria1003@gmail.com>

--- .oras/orasclient.py
+++ .oras/orasclient.py
@@ -7,7 +7,7 @@ input_value = input().split(";;")
 client.login(password=input_value[1], username=input_value[0])
 
 client.push(
-    name="ghcr.io/appthreat/blintdb-meson:v0.1",
+    target="ghcr.io/appthreat/blintdb-meson:v0.1",
     files=[
         ("./.oras/config.json", "application/vnd.oras.config.v1+json"),
         ("./.oras/annotations.json", "application/vnd.oras.annotation.v1+json"),
