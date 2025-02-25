82f7f5d61b061fc0bab7437b0caf7cdc979b9822
Prabhu Subramanian <prabhu@appthreat.com>
2025-02-25 13:29:00 +0000

    Switch to uv. Deleted unwanted directories
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- blint_db/handlers/git_handler.py
+++ blint_db/handlers/git_handler.py
@@ -4,20 +4,18 @@
 
 import subprocess
 
-from blint_db import DEBUG_MODE
-
 
 def git_clone(git_url, loc):
     # TODO: error checking here
     # TODO: handle and print output of command
     command = ["git", "clone", git_url, loc]
-    proc_output = subprocess.run(command, capture_output=True, check=False)
-    if DEBUG_MODE:
-        print(proc_output.stdout)
+    print (git_url, loc)
+    proc_output = subprocess.run(command, capture_output=True, check=False, encoding="utf-8")
+    print(proc_output.stdout)
 
 
 def git_checkout_commit(loc, commit_hash):
     command = ["git", "-C", loc, "checkout", commit_hash]
-    proc_output = subprocess.run(command, capture_output=True, check=False)
-    if DEBUG_MODE:
-        print(proc_output.stdout)
+    print (loc, commit_hash)
+    proc_output = subprocess.run(command, capture_output=True, check=False, encoding="utf-8")
+    print(proc_output.stdout)
